94ff1346093b68a1ce71e18aac4248acf92fd2c2
David Max <DavidJamesMax@Gmail.com>
2014-07-31 20:32:43 -0700

    added simple Next button for search result pagination

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -18,6 +18,13 @@ angular.module('govTrackrApp')
 
     });
 
+      $scope.page = 1;
+
+      $scope.nextPage = function() {
+        $scope.page++;
+        $scope.startSearch();
+      };
+
     console.log($scope.agencies);     
 
     $scope.selectedAgency = 'Choose An Agency';
@@ -30,8 +37,12 @@ angular.module('govTrackrApp')
       $scope.searchPerformed = true;
 
       $scope.agencyName = $scope.selectedAgency.substring(41, $scope.selectedAgency.length);
+
+      var sunURL = 'http://congress.api.sunlightfoundation.com/bills/search?query=FISA&apikey=b7caa92fa4364d9c961bcf7f950f5b40';
+
+
  
-      var url = 'https://www.federalregister.gov/api/v1/articles.json?callback=JSON_CALLBACK&per_page=10&order=relevance&conditions[term]=' + $scope.searchTerm + '&conditions[agencies][]=' + $scope.agencyName;
+      var url = 'https://www.federalregister.gov/api/v1/articles.json?callback=JSON_CALLBACK&per_page=10&order=relevance&conditions[term]=' + $scope.searchTerm + '&conditions[agencies][]=' + $scope.agencyName + '&page=' + $scope.page;
 
       $http.jsonp(url).success(function(data) {
     
@@ -40,12 +51,15 @@ angular.module('govTrackrApp')
       var description = data.description;
       
       var results = data.results;
+
      
       console.log(description);
       console.log($scope.searchResults);
 
       $scope.searchResults.results = results;
       $scope.searchResults.description = description;
+
+
 
     });
   };
