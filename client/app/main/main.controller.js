7e3b2b5fba022299f7919859d02d520d0090bb9b
David Max <DavidJamesMax@Gmail.com>
2014-08-02 21:20:41 -0700

    fixed ui.bootstrap pagination compatibility issue; began cleanup of code in main.controller.js

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,27 +3,27 @@
 angular.module('govTrackrApp')
   .controller('MainCtrl', function ($scope, $http) {
 
+// declare varialbles
     $scope.searchPerformed = false;
-
     $scope.searchResults = {};
-
     $scope.agencies = [];
+    $scope.currentPage = 1;
+    $scope.page = 1;
 
+// **obsolete** function for Next Page functionality
+    $scope.nextPage = function() {
+      $scope.page++;
+      $scope.startSearch();
+    };
 
+// get list of agencies
     $http.jsonp('https://www.federalregister.gov/api/v1/agencies?callback=JSON_CALLBACK').success(function(data) {
-    
-      $scope.agencies = data;
 
-        console.log(data[0].description);
+      $scope.agencies = data;
+      
+      console.log(data[0].description);
 
     });
-
-      $scope.page = 1;
-
-      $scope.nextPage = function() {
-        $scope.page++;
-        $scope.startSearch();
-      };
 
     console.log($scope.agencies);     
 
@@ -38,10 +38,10 @@ angular.module('govTrackrApp')
 
       $scope.agencyName = $scope.selectedAgency.substring(41, $scope.selectedAgency.length);
 
-      var sunURL = 'http://congress.api.sunlightfoundation.com/bills/search?query=FISA&apikey=b7caa92fa4364d9c961bcf7f950f5b40';
-
-
+      // URL for testing Sunlight Foundation API
+     // var sunURL = 'http://congress.api.sunlightfoundation.com/bills/search?query=FISA&apikey=b7caa92fa4364d9c961bcf7f950f5b40';
  
+ // assemble query string with params
       var url = 'https://www.federalregister.gov/api/v1/articles.json?callback=JSON_CALLBACK&per_page=10&order=relevance&conditions[term]=' + $scope.searchTerm + '&conditions[agencies][]=' + $scope.agencyName + '&page=' + $scope.currentPage;
 
       $http.jsonp(url).success(function(data) {
@@ -49,33 +49,30 @@ angular.module('govTrackrApp')
       $scope.searchResults = data;
 
       var description = data.description;
-      
       var results = data.results;
-
      
       console.log(description);
       console.log($scope.searchResults);
 
       $scope.searchResults.results = results;
       $scope.searchResults.description = description;
+      $scope.totalItems = data.count;
 
-  $scope.totalItems = data.count;
-  $scope.currentPage = 1;
+      $scope.displayTotalFedRegPages = data.total_pages;
 
-  $scope.setPage = function (pageNo) {
-    $scope.currentPage = pageNo;
-    $scope.startSearch();
-  };
+      $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+        $scope.startSearch();
+      };
 
-  $scope.pageChanged = function() {
-           $scope.startSearch();
-    console.log('Page changed to: ' + $scope.currentPage);
-  };
+      $scope.pageChanged = function() {
+      $scope.startSearch();
+      console.log('Page changed to: ' + $scope.currentPage);
+      };
 
-  $scope.maxSize = 5;
-  $scope.bigTotalItems = data.count;
-  $scope.bigCurrentPage = 1;
-
+      $scope.maxSize = 5;
+      $scope.bigTotalItems = data.count;
+      $scope.bigCurrentPage = 1;
 
     });
   };
